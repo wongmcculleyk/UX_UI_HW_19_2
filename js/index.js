fdd72c9e63d966bf32172b52af70591261b940da
wongmcculleyk <113258900+wongmcculleyk@users.noreply.github.com>
2022-10-12 09:57:20 -0500

    alert added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 console.log("Your index.js file is loaded correctly!");
-$("button").on("click", function() {
-    console.log("You clicked on a button");
+$("#btn").on("click", function() {
+    alert("You clicked on a button");
 }); 
